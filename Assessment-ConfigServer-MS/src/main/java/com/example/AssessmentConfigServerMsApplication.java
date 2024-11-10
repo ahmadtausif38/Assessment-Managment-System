d59682821ce6b153ce0482bf764a9d50f38ae63d
ahmadtausif38 <ahmadtausif38@gmail.com>
2024-11-10 23:06:58 +0530

    Secured Github Repo Token, modify logout logic in API Gateway

--- Assessment-ConfigServer-MS/src/main/java/com/example/AssessmentConfigServerMsApplication.java
+++ Assessment-ConfigServer-MS/src/main/java/com/example/AssessmentConfigServerMsApplication.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -9,7 +11,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class AssessmentConfigServerMsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AssessmentConfigServerMsApplication.class, args);
-	}
+        SpringApplication.run(AssessmentConfigServerMsApplication.class, args);
+    }
+
 
 }
